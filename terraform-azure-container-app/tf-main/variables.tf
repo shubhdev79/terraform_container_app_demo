d600d47bb12a557385f598460f2148032dd0bb9b
Shubham Dixit <51513309+shubhdev79@users.noreply.github.com>
2025-05-08 13:01:09 +0530

    Update variables.tf

--- terraform-azure-container-app/tf-main/variables.tf
+++ terraform-azure-container-app/tf-main/variables.tf
@@ -1,9 +1,11 @@
+# These will get override in tf-env/main.tf
+
 variable "app_required" {
    type = bool
    default = false
 }
 
-variable "env" {
+variable "env" { 
     type = string
     description = "DEV,TEST, REF & PROD"
 }
