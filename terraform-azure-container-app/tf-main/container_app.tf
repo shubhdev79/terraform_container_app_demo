edc081baaedbaae1416f05f496b6c6d6def83ad7
Shubham Dixit <51513309+shubhdev79@users.noreply.github.com>
2025-05-08 13:00:01 +0530

    Update container_app.tf

--- terraform-azure-container-app/tf-main/container_app.tf
+++ terraform-azure-container-app/tf-main/container_app.tf
@@ -1,3 +1,8 @@
+
+#This module will talk to the tf-modules and get the values
+
+# Using this variable => app_required ( True/False ) in case we multiple regions and envs and we dont want the container app in one of it.
+
 module "container_app" {
   count                        = var.app_required ? 1:0
   source                       = "../tf-modules/container_app"
